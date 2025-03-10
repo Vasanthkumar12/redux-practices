2561acfaed87e418326c43ceca184eeb8b4c88f6
Vasanthkumar M <vasanth23402@gmail.com>
2025-03-10 21:30:16 +0530

    Step 3 is done. Provide the store to all components

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { postStore } from '../redux/store.js'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <Provider store={postStore} >
+        <App />
+    </Provider>
 )
+
+
+// Step 1: Wrap the App component by Provider component.
+// Step 2: import Provider from react-reduct library.
+// Step 3: pass the postStore(which is the variable created in store.js file using createStore method )
