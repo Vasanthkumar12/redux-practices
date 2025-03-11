25f823a23f458050f74886a596f7bad95f8e5a83
Vasanthkumar M <vasanth23402@gmail.com>
2025-03-11 09:30:18 +0530

    Added the Redux dev tool extension

--- redux/store.js
+++ redux/store.js
@@ -1,4 +1,6 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { postReducer } from './postReducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-export const postStore = createStore(postReducer)
+export const postStore = createStore(postReducer, composeWithDevTools(
+    applyMiddleware()))
